ac4126d15fb208c90b025a8bba4cfb7e0df6b6b8
ThatCoderKid726 <83380065+ThatCoderKid726@users.noreply.github.com>
2021-10-02 09:22:17 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDfufjJj_rPvtVqsTyjk3scqbI6wMKjv_c",
+    authDomain: "kwitter-project-7bc4d.firebaseapp.com",
+    databaseURL: "https://kwitter-project-7bc4d-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-7bc4d",
+    storageBucket: "kwitter-project-7bc4d.appspot.com",
+    messagingSenderId: "350036597528",
+    appId: "1:350036597528:web:bfd55b1208b88953ba5c6d"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+     Room_names = childKey;
+    //Start code
+
+    //End code
+    });});}
+getData();
